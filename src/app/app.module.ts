03acde7f460595d1abf4f6ad597a30d7dd87218a
Sundarabharathi <sundarabharathi996@gmail.com>
2023-05-15 19:32:59 +0530

    Doctor Appointment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppointmentListComponent } from './appointment-list/appointment-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentComponent,
+    AppointmentListComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
